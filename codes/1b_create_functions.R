69818b49c8df793c416d9b3b747530af695c54d0
Alysson Portella <44845139+alyssonlp@users.noreply.github.com>
2024-06-17 16:02:15 -0300

    Update 1b_create_functions.R

--- codes/1b_create_functions.R
+++ codes/1b_create_functions.R
@@ -6,7 +6,7 @@
  # grupo - genero, raca, idade, ocupacao
  
  med_conj_desag_fun <- function(data_base, renda, peso, grupo) {
-  library(data.table) 
+  library(data.table) # BIBLIOTECAS CARREGADAS APENAS NO MAIN!
   library(questionr)
    
    {{data_base}}[, list(avg_wage = wtd.mean(get(renda), weights = get(peso))),
@@ -26,7 +26,7 @@
 massa_salarial_fun <- function(data_base, salario_medio){
   library(data.table)
   trabalhadores <- data_base[VD4002 == 1, .(soma_peso = sum(V1028))]
-  trabalhadores[, soma_peso := soma_peso * salario_medio]
+  trabalhadores[, soma_peso := soma_peso * salario_medio] # ISSO AQUI ESTA CERTO? ME PARECE ARRISCADO.
    
 }
 
@@ -38,3 +38,5 @@ delta_interanual_fun <- function(data_base, categoria) {
   delta_interanual <- aggregate(avg_wage ~ categoria, data = as.data.frame(data_base), 
                                      FUN = function(x) (((x[2] - x[1]) / x[1]) * 100)) 
 }
+
+# EU TINHA IMAGINADO ESSE PROCESSO UM POUCO DIFERENTE. FAZER UM UMA FUNÇÃO QUE DEPENDENDESSE DO ANO E TRIMESTRE E COMEÇARIA CARREGANDO A ASE DE T E T -1. DAI CALCULARIA TUDO QUE PRECISA SER CALCULADO E EXPORTARIA PARA UM RDS OU CSV. MAS PODEMOS TESTAR SE ISSO FUNCIONA
