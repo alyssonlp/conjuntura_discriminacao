7d75ac0a95cdd4fb0883ad8a4bebd2aa46f305c0
Rodrigo Carvalho <rodrigo1995csilva@gmail.com>
2024-07-03 18:24:35 -0300

    #4 massa salarial perdida pelas mulheres

--- codes/2b_massa_perdida.R
+++ codes/2b_massa_perdida.R
@@ -1,8 +1,8 @@
 dt1 <- data.table()
 
 # criando vetores para automatizar os arquivos conforme o ano e o trimestre
-ano <- c(2023)
-trimestre <- c(1)
+ano <- c(2012:2024)
+trimestre <- c(1:4)
 
 # no sprintf %d sao para valores inteiros e %s para nomes(string)
 # isso é similar ao `' do stata no looping
@@ -16,6 +16,7 @@ for(aa in ano) {
     
     rds_file <- sprintf("pnadc%d_%d_carta.rds", aa, tri)
     dt <- readRDS((file.path(original_data, rds_file)))
+
   
     # calculando a media do conjunto var_x - adicionar ocupacoes
     var_x <- c("educ", "educ_sq", "educ_cubic", "educ_fourth", "nonwhite",  "potential_xp" ,
@@ -39,7 +40,7 @@ for(aa in ano) {
                "ES" , "RJ" , "SP" , "PR" , "SC" , "RS" , "MS" , "MT" ,"GO")
     
     # Renda - homens e mulheres
-    modelo_salarial <- feols(ln_r_hab_all ~ female +.[var_x], 
+    modelo_salarial <- feols(ln_r_hab_all  ~ male +.[var_x], 
                              data = dt, weights = ~V1028, fsplit = ~ female)
     coefs <- coef(modelo_salarial)
     
@@ -52,28 +53,26 @@ for(aa in ano) {
     size_x <-length(var_x)
     matrix_betas <- as.matrix(coefs)
     betas <- as.matrix(as.numeric(matrix_betas[1,6:(size_x+5)]))
-    
-    # Salario esperado para mulheres : E[yi|R=1] = a + E[X|R=1]b + gama
-    matrix_x_mulher <- as.matrix(gender_media_x[2,2:(size_x+1)])
-    
-    alfa_mulher <- as.matrix(as.numeric(matrix_betas[3,4]))
-    gama <- as.matrix(as.numeric(matrix_betas[1,5]))
-
-    
-    e_wg_mulher <- alfa_mulher + (matrix_x_mulher%*%betas) - gama
+    alfa <- as.matrix(as.numeric(matrix_betas[1,4]))
     
     # Salario esperado para homens: E[yi|R=0] = a + E[X|R=0]b
     matrix_x_homem <- as.matrix(gender_media_x[1, 2:(size_x+1)])
-    alfa_homem <- as.matrix(as.numeric(matrix_betas[2,4]))
     
-    e_wg_homem <- alfa_homem + (matrix_x_homem%*%betas)
+    e_wg_homem <- alfa + (matrix_x_homem%*%betas)
     
+
+    # Salario esperado para mulheres : E[yi|R=1] = a + E[X|R=1]b + gama
+    matrix_x_mulher <- as.matrix(gender_media_x[2,2:(size_x+1)])
+    gama <- as.matrix(as.numeric(matrix_betas[1,5]))
+    gama <- gama *(-1)
     # Delta_x*beta = (E[X|R=1] - E[X|R=0])*beta
     Delta_X_beta <-  as.numeric((matrix_x_mulher - matrix_x_homem)%*%betas)
-    
+
+    e_wg_mulher <-  e_wg_homem + Delta_X_beta + gama
+  
     
     # Empregabilidade
-    modelo_empregabilidade <- feols(emp ~ female +  .[var_z], 
+    modelo_empregabilidade <- feols(emp ~ male +  .[var_z], 
                                     data = dt, weights = ~V1028, fsplit = ~ female)
     coefs_emp <- coef(modelo_empregabilidade)
     
@@ -86,23 +85,24 @@ for(aa in ano) {
     size_z <-length(var_z)
     matrix_phi <- as.matrix(coefs_emp)
     phi <- as.matrix(as.numeric(matrix_phi[1,6:(size_z+5)]))
-
-    # Probabilidade esperada de trabalhar para mulheres: P[Z|R= 1] = teta + E[Z|R=1]phi + lower_delta
-    matrix_z_mulher <- as.matrix(gender_media_z[2,2:(size_z+1)])
-    
-    teta_mulher <- as.matrix(as.numeric(matrix_phi[3,4]))
-    lower_delta <- as.matrix(as.numeric(matrix_phi[1,5]))
-    
-    p_emp_mulher <- teta_mulher + (matrix_z_mulher%*%phi) + lower_delta
+    teta <- as.matrix(as.numeric(matrix_phi[1,4]))
     
     # Probabilidade esperada de trabalhar para homens: P[Z|R=0] = teta + E[Z|R=0]phi
     matrix_z_homem <- as.matrix(gender_media_z[1, 2:(size_z+1)])
-    teta_homem <- as.matrix(as.numeric(matrix_phi[2,4]))
     
-    p_emp_homem <- teta_homem + (matrix_z_homem%*%phi)
+    p_emp_homem <- teta + (matrix_z_homem%*%phi)
     
+ 
+    # Probabilidade esperada de trabalhar para mulheres: P[Z|R= 1] = teta + E[Z|R=1]phi + lower_delta
+    matrix_z_mulher <- as.matrix(gender_media_z[2,2:(size_z+1)])
+    lower_delta <- as.matrix(as.numeric(matrix_phi[1,5]))
+    lower_delta <- lower_delta*(-1)
     # Delta_z*phi = (P[Z|R=1] - P[Z|R=0])*phi
     Delta_Z_phi <-  as.numeric((matrix_z_mulher - matrix_z_homem)%*%phi)
+    
+    
+    p_emp_mulher <- p_emp_homem + Delta_Z_phi + lower_delta
+    
     
     # Massa salarial perdida
     # e_wg_mulher*(Delta_Z_phi + lower_delta)*M_1 + p_emp_homem*M_1*(Delta_X_beta + gama)
@@ -113,10 +113,10 @@ for(aa in ano) {
     
     massa_perdida_mulher <- (exp(e_wg_mulher))*(Delta_Z_phi + lower_delta)*M_1 + p_emp_homem*M_1*(Delta_X_beta + gama)
     salario_perdido_mulher <- (exp(e_wg_mulher))*(Delta_Z_phi + lower_delta)*M_1 
-    massa_perdida_emp_equivalente <- mass_perdida_mulher - salario_perdido_mulher
+    massa_perdida_emp_equivalente <- massa_perdida_mulher - salario_perdido_mulher
   
     massa_perdida <- c("massa_perdida_mulher")
-    salario_perdido_devido_menor_p_emp <- C("salario_perdido_mulher")
+    salario_perdido_devido_menor_p_emp <- c("salario_perdido_mulher")
     massa_perdida_emp_equivalente_devido_discr <- c("massa_perdida_emp_equivalente")
     anotri <- sprintf("%dT%d", aa, tri)
     dt2 <- data.table(Ano_trimestre = anotri, massa_perdida = massa_perdida_mulher,
