938881b4f9a419ddfb9d3983995bca0701bfda46
Rodrigo Carvalho <rodrigo1995csilva@gmail.com>
2024-07-04 17:42:48 -0300

    Update 2b_massa_perdida.R
    #4

--- codes/2b_massa_perdida.R
+++ codes/2b_massa_perdida.R
@@ -13,13 +13,12 @@ for(aa in ano) {
     if(aa == 2024 & tri >=2){
       next  
     }
-    
     rds_file <- sprintf("pnadc%d_%d_carta.rds", aa, tri)
     dt <- readRDS((file.path(original_data, rds_file)))
 
   
-    # calculando a media do conjunto var_x - adicionar ocupacoes
-    var_x <- c("educ", "educ_sq", "educ_cubic", "educ_fourth", "nonwhite",  "potential_xp" ,
+    # calculando a media do conjunto var_x 
+    var_x <- c("nonwhite", "educ", "educ_sq",  "potential_xp" ,
                "potential_xp_sq","rural" , "metropolitan" ,"diretores_gerentes", "ciencias_intelectuais", 
                "tec_nivel_medio", "apoio_adm", "servicos_comercio", "agropecuaria_pesca", 
                "operarios_construcao", "maquinas_montadores", "ocup_elementares", "ffaa",
@@ -31,100 +30,144 @@ for(aa in ano) {
                "RO" , "AC" , "AM" , "RR" , "PA"  , "AP" , 'TO' , "MA" , "PI" , 'CE' ,
                "RN" , "PB" , "PE" , "AL" , "SE" ,  "BA" , "MG" , "ES" , "RJ" , "SP" , 
                "PR" , "SC" , "RS" , "MS" , "MT" , "GO")
+    size_x <-length(var_x)
     
     # calculando a media do conjunto var_z
-    var_z <- c("educ" , "educ_sq" , "educ_cubic", "educ_fourth", "nonwhite" , "potential_xp" ,
+    var_z <- c("nonwhite" ,  "mother" ,"educ" , "educ_sq" , "potential_xp" ,
                "potential_xp_sq",  "married" , "rural" , "metropolitan" ,
-               "mother" , "RO" , "AC" , "AM" , "RR" , "PA" , "AP" , "TO" , "MA" , 
+               "RO" , "AC" , "AM" , "RR" , "PA" , "AP" , "TO" , "MA" , 
                "PI" , "CE" , "RN" , "PB" , "PE" , "AL" , "SE" , "BA" , "MG" ,
                "ES" , "RJ" , "SP" , "PR" , "SC" , "RS" , "MS" , "MT" ,"GO")
+    size_z <- length(var_z)
     
-    # Renda - homens e mulheres
-    modelo_salarial <- feols(ln_r_hab_all  ~ male +.[var_x], 
-                             data = dt, weights = ~V1028, fsplit = ~ female)
-    coefs <- coef(modelo_salarial)
+    # Modelo Salarial
+    modelo_salarial <- feols(ln_r_hab_all  ~ .[var_x], 
+                             data = dt, weights = ~V1028, split = ~ female)
     
-    gender_media_x <- dt[, lapply(.SD, function(x) wtd.mean(x, weights = V1028, na.rm = TRUE)), 
-                       by = .(female), .SDcols = var_x]
-    gender_media_x[, female := as.numeric(female)]
+    beta_homem <- modelo_salarial[[1]]$coefficients 
+    intercepto_homem <- as.matrix(beta_homem[[1]])
+    gama_homem <- as.matrix(beta_homem[[2]])
+    matrix_beta_homem <-  as.matrix(beta_homem) 
     
+    beta_mulher <- modelo_salarial[[2]]$coefficients
+    intercepto_mulher <- as.matrix(beta_mulher[[1]])
+    gama_mulher <- as.matrix(beta_mulher[[2]])
+    matrix_beta_mulher <- as.matrix(beta_mulher)
     
-    # salario esperado
-    size_x <-length(var_x)
-    matrix_betas <- as.matrix(coefs)
-    betas <- as.matrix(as.numeric(matrix_betas[1,6:(size_x+5)]))
-    alfa <- as.matrix(as.numeric(matrix_betas[1,4]))
+    # Media das covariadas do modelo salarial
+    gender_race_media_x <- dt[, lapply(.SD, function(x) wtd.mean(x, weights = V1028, na.rm = TRUE)), 
+                              by = .(gender_race), .SDcols = var_x]
     
-    # Salario esperado para homens: E[yi|R=0] = a + E[X|R=0]b
-    matrix_x_homem <- as.matrix(gender_media_x[1, 2:(size_x+1)])
+    filtro_hb <- gender_race_media_x %>%  filter(gender_race == "Homem Branco")
+    filtro_hn <- gender_race_media_x %>%  filter(gender_race == "Homem Negro")
+    filtro_mb <- gender_race_media_x %>%  filter(gender_race == "Mulher Branca")
+    filtro_mn <- gender_race_media_x %>%  filter(gender_race == "Mulher Negra")
     
-    e_wg_homem <- alfa + (matrix_x_homem%*%betas)
+    matrix_x_hb <- as.matrix(filtro_hb[, 3:(size_x+1)])
+    matrix_x_hn <-  as.matrix(filtro_hn[, 3:(size_x+1)])
     
-
-    # Salario esperado para mulheres : E[yi|R=1] = a + E[X|R=1]b + gama
-    matrix_x_mulher <- as.matrix(gender_media_x[2,2:(size_x+1)])
-    gama <- as.matrix(as.numeric(matrix_betas[1,5]))
-    gama <- gama *(-1)
-    # Delta_x*beta = (E[X|R=1] - E[X|R=0])*beta
-    Delta_X_beta <-  as.numeric((matrix_x_mulher - matrix_x_homem)%*%betas)
-
-    e_wg_mulher <-  e_wg_homem + Delta_X_beta + gama
+    matrix_x_mb <-  as.matrix(filtro_mb[, 3:(size_x+1)])
+    matrix_x_mn <-  as.matrix(filtro_mn[, 3:(size_x+1)])
+    
+    # Salario Esperado - Homens
+    # homem branco: E[yi|R=0] = a + E[X|R=0]b
+    e_wg_hb <- intercepto_homem + matrix_x_hb%*%matrix_beta_homem[3:(size_x+1),]
+    
+    # homem negro: E[yi|R =1] = E[yi|R=0] + delta_x_beta + gama
+    delta_x_beta_homem <- (matrix_x_hn - matrix_x_hb)%*%matrix_beta_homem[3:(size_x+1),]
+    e_wg_hn <- e_wg_hb + delta_x_beta_homem + gama_homem
+    
+    # Salario Esperado - Mulheres
+    # mulher branca: E[yi|R=0] = a + E[X|R=0]b
+    e_wg_mb <- intercepto_mulher + matrix_x_mb%*%matrix_beta_mulher[3:(size_x+1),]
+    
+    # mulher negra: E[yi|R =1] = E[yi|R=0] + delta_x_beta + gama
+    delta_x_beta_mulher<- (matrix_x_mn - matrix_x_mb)%*%matrix_beta_mulher[3:(size_x+1),]
+    e_wg_mn <- e_wg_mb + delta_x_beta_mulher + gama_mulher
   
     
-    # Empregabilidade
-    modelo_empregabilidade <- feols(emp ~ male +  .[var_z], 
-                                    data = dt, weights = ~V1028, fsplit = ~ female)
-    coefs_emp <- coef(modelo_empregabilidade)
+    # Modelo Empregabilidade
+    modelo_empregabilidade <- feols(emp  ~ .[var_z], 
+                                    data = dt, weights = ~V1028, split = ~ female)
     
-    gender_media_z <- dt[, lapply(.SD, function(x) wtd.mean(x, weights = V1028, na.rm = TRUE)), 
-                         by = .(female), .SDcols = var_z]
-    gender_media_z[, female := as.numeric(female)]
+    phi_homem <- modelo_empregabilidade[[1]]$coefficients 
+    intercepto_emp_homem <- as.matrix(phi_homem[[1]])
+    lower_delta_homem <- as.matrix(phi_homem[[2]])
+    matrix_phi_homem <-  as.matrix(phi_homem) 
     
+    phi_mulher <- modelo_empregabilidade[[2]]$coefficients
+    intercepto_emp_mulher <- as.matrix(phi_mulher[[1]])
+    lower_delta_mulher <- as.matrix(phi_mulher[[2]])
+    matrix_phi_mulher <- as.matrix(phi_mulher)
     
-    # Probabilidade em trabalhar
-    size_z <-length(var_z)
-    matrix_phi <- as.matrix(coefs_emp)
-    phi <- as.matrix(as.numeric(matrix_phi[1,6:(size_z+5)]))
-    teta <- as.matrix(as.numeric(matrix_phi[1,4]))
+    # Media das covariadas do modelo empregabilidade
+    gender_race_media_z <- dt[, lapply(.SD, function(x) wtd.mean(x, weights = V1028, na.rm = TRUE)), 
+                              by = .(gender_race), .SDcols = var_z]
     
-    # Probabilidade esperada de trabalhar para homens: P[Z|R=0] = teta + E[Z|R=0]phi
-    matrix_z_homem <- as.matrix(gender_media_z[1, 2:(size_z+1)])
-    
-    p_emp_homem <- teta + (matrix_z_homem%*%phi)
-    
- 
-    # Probabilidade esperada de trabalhar para mulheres: P[Z|R= 1] = teta + E[Z|R=1]phi + lower_delta
-    matrix_z_mulher <- as.matrix(gender_media_z[2,2:(size_z+1)])
-    lower_delta <- as.matrix(as.numeric(matrix_phi[1,5]))
-    lower_delta <- lower_delta*(-1)
-    # Delta_z*phi = (P[Z|R=1] - P[Z|R=0])*phi
-    Delta_Z_phi <-  as.numeric((matrix_z_mulher - matrix_z_homem)%*%phi)
+    filtro_hb_emp <- gender_race_media_z %>%  filter(gender_race == "Homem Branco")
+    filtro_hn_emp <- gender_race_media_z %>%  filter(gender_race == "Homem Negro")
+    filtro_mb_emp <- gender_race_media_z %>%  filter(gender_race == "Mulher Branca")
+    filtro_mn_emp <- gender_race_media_z %>%  filter(gender_race == "Mulher Negra")
     
     
-    p_emp_mulher <- p_emp_homem + Delta_Z_phi + lower_delta
+    matrix_z_hb <- as.matrix(filtro_hb_emp[, 4:(size_z+1)])
+    matrix_z_hn <- as.matrix(filtro_hn_emp[, 4:(size_z+1)])
     
+    matrix_z_mb <-  as.matrix(filtro_mb_emp[, 3:(size_z+1)])
+    matrix_z_mn <-  as.matrix(filtro_mn_emp[, 3:(size_z+1)])
     
-    # Massa salarial perdida
-    # e_wg_mulher*(Delta_Z_phi + lower_delta)*M_1 + p_emp_homem*M_1*(Delta_X_beta + gama)
+    # Probabilidade em estar empregado - Homens
+    # homem branco: P[Z|R=0] = teta + E[Z|R=0]phi
+    p_emp_hb <- intercepto_emp_homem + matrix_z_hb%*%matrix_phi_homem[3:(size_z),]
     
-    # M_1 = mulheres na economia
+    # homem negro: P[Z|R =1] = P[Z|R=0] + delta_z_phi + lower_delta
+    delta_z_phi_homem <- (matrix_z_hn - matrix_z_hb)%*%matrix_phi_homem[3:(size_z),]
+    p_emp_hn <- p_emp_hb + delta_z_phi_homem + lower_delta_homem
     
-    M_1 <- sum(dt$V1028[dt$female == 1])
+    # Probabilidade em estar empregada  - Mulheres
+    # mulher branca: P[Z|R=0] = teta + E[Z|R=0]phi
+    p_emp_mb <- intercepto_emp_mulher + matrix_z_mb%*%matrix_phi_mulher[3:(size_z+1),]
     
-    massa_perdida_mulher <- (exp(e_wg_mulher))*(Delta_Z_phi + lower_delta)*M_1 + p_emp_homem*M_1*(Delta_X_beta + gama)
-    salario_perdido_mulher <- (exp(e_wg_mulher))*(Delta_Z_phi + lower_delta)*M_1 
-    massa_perdida_emp_equivalente <- massa_perdida_mulher - salario_perdido_mulher
+    # mulher negra: P[Z|R =1] = P[Z|R=0] + delta_z_phi + lower_delta
+    delta_z_phi_mulher<- (matrix_z_mn - matrix_z_mb)%*%matrix_phi_mulher[3:(size_z+1),]
+    p_emp_mn <- p_emp_mb + delta_z_phi_mulher + lower_delta_mulher
+    
+    # Massa salarial perdida - homens negros em relacao aos homens brancos
+    # e_wg_hb*(delta_z_phi_homem + lower_delta_homem)*M_hn + p_emp_hn*M_hn*(delta_x_beta_homem + gama_homem)
+    
+    # M_hn = homens negros na economia
+    
+    M_hn <- sum(dt$V1028[dt$gender_race == "Homem Negro"])
+    
+    massa_perdida_hn <- exp(e_wg_hb)*(delta_z_phi_homem + lower_delta_homem)*M_hn +
+      p_emp_hn*M_hn*(delta_x_beta_homem + gama_homem)
+    
+    # Massa salarial perdida - mulheres negras em relacao as mulheres brancas
+    # e_wg_mb*(delta_z_phi_mulher + lower_delta_mulher)*M_mn + p_emp_mn*M_mn*(delta_x_beta_mulher + gama_mulher)
+    
+    # M_mn = mulheres negras na economia
+    
+    M_mn <- sum(dt$V1028[dt$gender_race == "Mulher Negra"])
+    
+    massa_perdida_mn <- exp(e_wg_mb)*(delta_z_phi_mulher + lower_delta_mulher)*M_mn +
+      p_emp_mn*M_mn*(delta_x_beta_mulher + gama_mulher)
   
-    massa_perdida <- c("massa_perdida_mulher")
-    salario_perdido_devido_menor_p_emp <- c("salario_perdido_mulher")
-    massa_perdida_emp_equivalente_devido_discr <- c("massa_perdida_emp_equivalente")
+    
     anotri <- sprintf("%dT%d", aa, tri)
-    dt2 <- data.table(Ano_trimestre = anotri, massa_perdida = massa_perdida_mulher,
-                      salario_perdido_devido_menor_p_emp = salario_perdido_mulher,
-                      massa_perdida_emp_equivalente_devido_discr = massa_perdida_emp_equivalente)
+    dt2 <- data.table(Ano_trimestre = anotri, 
+                      salario_esperado_medio_homem_branco = e_wg_hb,
+                      salario_esperado_medio_homem_negro = e_wg_hn,
+                      salario_esperado_medio_mulher_branca = e_wg_mb,
+                      salario_esperado_medio_mulher_negra = e_wg_mn,
+                      probabilidade_emprego_medio_homem_branco = p_emp_hb,
+                      probabilidade_emprego_medio_homem_negro = p_emp_hn,
+                      probabilidade_emprego_medio_mulher_branca = p_emp_mb,
+                      probabilidade_emprego_mulher_negra = p_emp_mn,
+                      massa_salarial_perdida_homem_negro = massa_perdida_hn,
+                      massa_salarial_perdida_mulher_negra = massa_perdida_mn)
     
     dt1 <- rbind(dt1, dt2, fill = TRUE)
     }
 }
 
-fwrite(dt1, file.path(csv_files, "massa_salarial_perdida_genero.csv"))
+fwrite(dt1, file.path(csv_files, "massa_salarial_perdida_genero_raca.csv"))
