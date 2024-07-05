f6733ec2cb21266dd6806851b57defee4a8b8f9f
Rodrigo Carvalho <rodrigo1995csilva@gmail.com>
2024-07-05 11:52:34 -0300

    Update main.R
    #2

--- codes/main.R
+++ codes/main.R
@@ -36,19 +36,24 @@ list_objects_to_keep <- c("datawork_folder", "github_folder", "codes",
                           "csv_files", "final_data")
 
 if (part_01 == TRUE ){
+  # limpeza e estatistica descritiva
    source(file.path(codes, "1a_clean_data_carta_replicacao.R"))
    source(file.path(codes, "1b_function_carta_conjuntura.R"))
    source(file.path(codes, "1c_resultados_carta_conjuntura.R"))
    source(file.path(codes, "1d_graficos_carta_conjuntura.R"))
    source(file.path(codes, "1e_graficos_carta_conjuntura_raca.R"))
    source(file.path(codes, "1f_graficos_carta_conjuntura_genero_raca.R"))
+   source(file.path(codes, "1g_graficos_labor_recente_gph.R"))
+   source(file.path(codes, "1h_frac_graficos_labor_recente.R"))
+   source(file.path(codes, "1i_top_bottom_labor_recente.R"))
 }
 
 if (part_02 == TRUE ){
-  # amostra 25-54 anos
-  source(file.path(codes, "2a_clean_data_conjuntura.R"))
-  source(file.path(codes, "2b_rendimento_medio_conjuntura.R"))
-  source(file.path(codes, "2c_modelos.R"))
+  # modelo econometrico
+  source(file.path(codes, "2a_hiatos.R"))
+  source(file.path(codes, "2b_massa_perdida.R"))
+  source(file.path(codes, "2c_massa_perdida_gph.R"))
+  source(file.path(codes, "2d_penalidade_gph.R"))
 }
 
 if (part_03 == TRUE ){
