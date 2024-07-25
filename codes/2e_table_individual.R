228af80c5a7d12d83c0f626d1b3a8e37fc64f47e
Rodrigo Carvalho <rodrigo1995csilva@gmail.com>
2024-07-25 15:38:23 -0300

    #5  apresentacao

--- codes/2e_table_individual.R
+++ codes/2e_table_individual.R
@@ -45,26 +45,25 @@ setnames(mulheres_e, c("2023T1", "2024T1"), c("Mulher_2023T1", "Mulher_2024T1"))
 
 individual_recente <- cbind(homens_e, mulheres_e)
 individual_recente <- individual_recente[, variaveis_m := NULL]
-individual_recente <- individual_recente[, Total_2023T1 := Homem_2023T1 + Mulher_2023T1]
-individual_recente <- individual_recente[, Total_2024T1 := Homem_2024T1 + Mulher_2024T1]
+
 individual_recente <- setcolorder(individual_recente,c("variaveis", "Homem_2023T1", "Mulher_2023T1",
-                                                       "Total_2023T1", "Homem_2024T1", "Mulher_2024T1", "Total_2024T1"))
+                                                        "Homem_2024T1", "Mulher_2024T1"))
 individual_recente <- setnames(individual_recente, c("variaveis", "Homem_2023T1", "Mulher_2023T1",
-                                                     "Total_2023T1", "Homem_2024T1", "Mulher_2024T1", "Total_2024T1"),
+                                                      "Homem_2024T1", "Mulher_2024T1"),
                                c("Resultados", "Homem 2023T1", "Mulher 2023T1",
-                                 "Total 2023T1", "Homem 2024T1", "Mulher 2024T1", "Total 2024T1"))
+                                  "Homem 2024T1", "Mulher 2024T1"))
 
 individual_recente <- individual_recente[c(5, 1:4, 6:nrow(individual_recente)), ]
 individual_recente <- individual_recente[1:5,]
 
 # Criando a tabela 
 table_individual <-
-  kable(head(individual_recente, 7), align = 'l', booktabs = TRUE, col.names = rep("", ncol(individual_recente))) %>% 
+  kable(head(individual_recente, 5), align = 'l', booktabs = TRUE, col.names = rep("", ncol(individual_recente))) %>% 
   column_spec(1, width = "300px") %>%  
-  column_spec(c(2:7), width = "100px") %>% 
+  column_spec(c(2:5), width = "100px") %>% 
   row_spec(1, bold = FALSE, background = "lightgray") %>% 
-  add_header_above(c("", "Homem" = 1, "Mulher" = 1, "Total" = 1, "Homem" = 1, "Mulher" = 1, "Total" = 1)) %>% 
-  add_header_above(c("", "2023" = 3, "2024" = 3)) %>% 
+  add_header_above(c("", "Homem" = 1, "Mulher" = 1,  "Homem" = 1, "Mulher" = 1)) %>% 
+  add_header_above(c("", "2023" = 2, "2024" = 2)) %>% 
   pack_rows("Diferença Salarial", 2, 3) %>% 
   pack_rows("Diferença na empregabilidade", 4, 5) 
 
